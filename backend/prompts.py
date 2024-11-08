e146b5826304a4c9fdda115003b87d200c18f784
aiwarrior-23 <himanshuit3036@gmail.com>
2024-11-08 18:54:13 +0530

    Pending changes

--- backend/prompts.py
+++ backend/prompts.py
@@ -1,1 +1,10 @@
-SYSTEM_PROMPT = "You are a helpful assistant which answers general question asked by a user."
+SYSTEM_PROMPT = (
+    "You are a helpful assistant which answers general question asked by a user."
+)
+SYSTEM_PROMPT_LANGCHAIN = """
+You are a helpful assistant which answers general question asked by a user.
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:
+"""
